cc7062b7bac8fb9d5bdb5333427f448750f79d9b
suki309 <sukisukanya5544@gmail.com>
2024-07-21 11:20:20 +0530

    terraform2

--- foreach/provider.tf
+++ foreach/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "5.56.1"
     }
   }
+  backend "s3"{
+    bucket = "roboshop1"
+    key = "for_each-demo"
+    region = "us-east-1"
+    dynamodb_table = "roboshop-locking"
+  }
 }
 
 provider "aws" {
